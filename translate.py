b8a93b6f715540f0a3330948abd021177fe7c5b7
usmaniarmaan281-cyber <usmaniarmaan281@gmail.com>
2025-11-15 14:28:48 +0530

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from googletrans import Translator
+
+st.title("Translator (English ↔ Hindi)")
+
+text = st.text_input("Enter text")
+translator = Translator()
+
+if st.button("Translate"):
+    translated = translator.translate(text, dest="hi")
+    st.success(translated.text)
